b384e073d61f444789ab101cd381f62066257137
Esprits <lmignacca@outlook.com>
2026-02-27 10:03:29 -0500

    Had fun with pseudo-class states
    - WAS THAT THE BITE OF '87?????

--- js/components/w95-window.js
+++ js/components/w95-window.js
@@ -2,8 +2,12 @@
 // I did not know you could do that; it's pretty cool so far
 
 class W95Window extends HTMLElement {
+	// static observedAttributes = ["style"];
+
 	constructor() {
 		super();
+
+		this._internals = this.attachInternals(); // Makes states selectable in CSS (with :state())
 	}
 
 	connectedCallback() { // Called each time the element is added to the document
@@ -11,6 +15,8 @@ class W95Window extends HTMLElement {
 		var mouseDown = false;
 		var offset = [0, 0];
 		var mousePosition;
+
+		var audio = new Audio("./audio/golden_freddy.mp3");
 
 		win.classList.add("window-border");
 		addDOMElements(win);
@@ -23,11 +29,15 @@ class W95Window extends HTMLElement {
 
 			if (true) { // TODO Once the title bar is complete, add a verification that it's clicking in it
 				mouseDown = true;
+				win.dragged = true;
+				audio.play();
 			}
 		}, true);
 
 		document.addEventListener('mouseup', function(e) {
 			mouseDown = false;
+			win.dragged = false;
+			audio.pause();
 		}, true);
 
 		document.addEventListener('mousemove', function(e) { // Makes the window follow the mouse if the click is held down
@@ -49,6 +59,18 @@ class W95Window extends HTMLElement {
 
 	attributeChangedCallback(name, oldValue, newValue) { // Called when attributes are changed, added, removed, or replaced.
 		// Unused for now
+	}
+
+	get dragged() {
+		return this._internals.states.has("dragged");
+	}
+
+	set dragged(flag) {
+		if (flag) {
+			this._internals.states.add("dragged");
+		} else {
+			this._internals.states.delete("dragged");
+		}
 	}
 }
 
